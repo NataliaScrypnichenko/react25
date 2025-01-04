c2c489ec1b54ce9323897ab3c629742e76547f16
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-01-04 19:03:51 +0200

    закінчення 1.2 урока

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
+
+// function App() {
+//
+//   return (
+//     <>
+//         <MyComponent/>
+//         {
+//             MyComponent()
+//         }
+//
+//     </>
+//   )
+// }
 
 function App() {
 
   return (
     <>
-        hello okten!
+        <MyComponent text={'hello 1'}/>
+        <MyComponent text={'hello 2'}/>
+        <MyComponent text={'hello okten'}/>
+        {
+            // MyComponent({text:'hello 2'}) не використовуэмо його
+        }
+
     </>
   )
 }
-
 export default App
---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,51 @@
+//будуємо все те що є в файлі App.tsx
+// function MyComponent() {
+//     return <div>hello okten!</div>
+// }
+
+//після цього вказую що цей компонент спроможний до імпорту цього файлу.Ми працюємо в Купі розмежених
+// між собою файлів
+
+//export default MyComponent;
+//це означає що можу тепер в цьому файлі писати що потрібно, а не файлі App.tsx, тобто тут пишу слова, а App.tsx
+// пишу назву функції, і буде все те саме/тобто фц. імпортується в файл App.tsx.Але є нюанс якщо я декілька захочу щось
+// написати викликати, то буде декілька разів написано  hello okten!, тобто можна перевикористовувати це.Ми
+//можемо в App.tsx поставити {}(джейєксі) що означає варіант інтерпуляції, можемо писати js коди.
+// Можна викликати її як фц. {MyComponent()} і буде все те саме.
+//Компоненти потрібні в проєкті для того щоб ми могли збирати як конструктор
+
+// Компонент буде мати різний текст
+//  function MyComponent({text}:{text:string}) {
+//      return <div>{text}</div>
+//  }
+// export default MyComponent;
+//щоб дані передати  <MyComponent/> то  <MyComponent text={'hello 1'}/>,text={'hello 1'}оце і є аргумент
+//робимо дещо менше і пишемо правильно
+
+// import {FC} from "react";
+//
+// type MyComponentPropType={text:string}
+//
+// const MyComponent: FC<MyComponentPropType>=({text})=> {
+//      return <div>{text}</div>
+//  }//це правильна конструкція.
+//  //це називається факшен компонент.Якщо ми не хочемо мати ({text}:MyComponentPropType), а іменно MyComponentPropType
+// //потім ми добавляємо FunctionComponent(FC) і вони будуть мати свій тип це FC<MyComponentPropType>
+// export default MyComponent;
+
+
+//  щоб швидко написати таку фцю то пишемо rsc дає підсказку
+//import React from 'react'; це видаляємо
+
+ import {FC} from "react";
+type MyComponentPropType={text:string}
+
+const MyComponent:FC<MyComponentPropType> = ({text}) => {
+    return (
+        <div>
+            {text}
+        </div>
+    );
+};
+
+export default MyComponent;
