87ee39200b4704e1acd3f3fc1761b7f8decb491b
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-01-12 18:13:43 +0200

    task2 z5PFSTAK далі слідує)) UsersComponent.tsx зупинилась

---
+++ src/components/users-component/UsersComponent.tsx
@@ -0,0 +1,12 @@
+import {useEffect, useState} from "react";
+import {IUser} from "../../models/IUser.ts";
+
+export const UsersComponent = () => {
+    const [users, setUsers] = useState <IUser[]> ([]);
+
+    useEffect()
+
+    return (
+        <div></div>
+    )
+}
