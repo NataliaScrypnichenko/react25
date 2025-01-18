1b5c857412b7f110edf0c163de5bb032f44733df
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-01-18 21:45:42 +0200

    5.1-не контрольовані і конт.компоненти.froms uncontrolled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-const App=() => {
+import {FC} from "react";
+import './App.css'
+import FromComponent from "./componets/FromComponent.tsx";
+
+ const App:FC=() => {
 
   return (
-    <div>
+      <div>
+        <FromComponent/>
 
-    </div>
+      </div>
   );
 };
-
-export default App
+export default App;
+//не котрольованому можна на сайті додавати все що хочеш, тому що АРР не конт.
+// не контр. моменти це форми
+{/*<div contentEditable={true} > Lorem ipsum dolor sit amet, consectetur adipisicing elit.*/}
+{/*  Accusamus accusantium at atque aut consequatur consequuntur culpa harum minima,*/}
+{/*  minus natus nemo nobis, odio quae, qui quos sit sunt veniam veritatis.</div>*/}
---
+++ src/componets/FromComponent.tsx
@@ -0,0 +1,15 @@
+
+const FromComponent = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+            </form>
+
+        </div>
+
+    );
+};
+
+export default FromComponent;
