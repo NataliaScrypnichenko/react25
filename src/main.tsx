c12d7cf0833975f9b8f473931a6ed3d3b44b3055
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-01-29 23:35:31 +0200

    8 потрібно ще робити

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,30 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {Provider} from "react-redux";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import App from "./App.tsx";
+import UsersPage from "./pages/UsersPage.tsx";
+import PostsPage from "./pages/PostsPage.tsx";
+import CommentsPage from "./pages/CommentsPage.tsx";
+import ComplexPage from "./pages/ComplexPage.tsx";
 
-createRoot(document.getElementById('root')!).render(<App />)
+const routers = createBrowserRouter([
+    {
+        path: "/",element:<App/>,children:[
+            {path:'users',element:<UsersPage/>},
+            {path:'posts',element:<PostsPage/>},
+            {path:'comments',element:<CommentsPage/>},
+            {path:'complex',element:<ComplexPage/>}
+        ]
+    }
+
+]);
+
+
+
+
+createRoot(document.getElementById('root')!).
+render(<Provider store={store}>
+    <RouterProvider router={routers}/>
+
+</Provider>)
